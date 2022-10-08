ca5b931831cbda38279244851690df32d9ff594b
ZiadSheriif <zsherif308@gmail.com>
2022-10-08 14:39:25 +0200

    feat: components of fan

--- main.c
+++ main.c
@@ -1,17 +1,26 @@
 /*
- * Module : Integeration Drivers
+ * Module : Integration Drivers
  *
  *
  * File Name: main.c
  *
- * Description: Soruce file which integerate all drivers with each other
+ * Description: Source file which integrate all drivers with each other
  *
  * Author: Ziad-Sherif
+ */
+
+
+/*
+ * Include all header files of driver
  */
 #include "lcd.h"
 #include "dc-motor.h"
 #include "adc.h"
 #include "pwm.h"
+#include"lm35_sensor.h"
+
+
 int main(void)
 {
+
 }
